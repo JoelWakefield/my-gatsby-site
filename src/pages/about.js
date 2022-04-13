6df5f2b1cfa61a9f84c1fd85db28022cfb0f87b6
Joel Wakefield <joel.wakefield@improving.com>
2022-04-13 12:09:44 -0500

    added about me page

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const AboutPage = (props) => {
+  return (
+    <main>
+      <title>About Me</title>
+      <h1>ME</h1>
+      <p>Hi there! I'm a maker!</p>
+    </main>
+  );
+};
+
+export default AboutPage;
