0b391a37edaa3a6411cf96b7841369a5cd065c8a
Joel Wakefield <joel.wakefield@improving.com>
2022-04-13 13:21:50 -0500

    more styling

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,23 +1,34 @@
 import React from "react";
 import { Link } from "gatsby";
-import { container } from "./layout.module.css";
+import {
+  container,
+  pageStyles,
+  heading,
+  navLinks,
+  navLinkItem,
+  navLinkText,
+} from "./layout.module.css";
 
 const Layout = ({ pageTitle, children }) => {
   return (
     <div className={container}>
       <title>{pageTitle}</title>
       <nav>
-        <ul style={listStyles}>
-          <li>
-            <Link to="/">Home</Link>
+        <ul className={navLinks}>
+          <li className={navLinkItem}>
+            <Link to="/" className={navLinkText}>
+              Home
+            </Link>
           </li>
-          <li>
-            <Link to="/about">About</Link>
+          <li className={navLinkItem}>
+            <Link to="/about" className={navLinkText}>
+              About
+            </Link>
           </li>
         </ul>
       </nav>
-      <main style={pageStyles}>
-        <h1>{pageTitle}</h1>
+      <main className={pageStyles}>
+        <h1 className={heading}>{pageTitle}</h1>
         {children}
       </main>
     </div>
