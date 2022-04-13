4789a0b09d9efc3c1abc66c92e5efbd01c41dc8b
Joel Wakefield <joel.wakefield@improving.com>
2022-04-13 14:09:52 -0500

    added plugins for images

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,16 @@
 import * as React from "react";
 import Layout from "../components/layout";
+import { StaticImage } from "gatsby-plugin-image";
 
-// markup
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
-      <h1>Welcome!</h1>
+      <h2>Welcome!</h2>
+      <p>This is my website.</p>
+      <StaticImage
+        alt="He-Man: fabulous powers included ;)"
+        src="../images/he-man.webp"
+      />
     </Layout>
   );
 };
